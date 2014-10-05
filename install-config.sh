22df83b6312b60d773561ee0d5be5f5fa060c7da
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2014-10-05 17:41:58 -0400

    Added basic hgrc.

--- install-config.sh
+++ install-config.sh
@@ -5,6 +5,10 @@ myexit() {
     exit 1
 }
 
+[ -e "~/.hgrc" ] && myexit "~/.hgrc already exists..."
+
+ln -s $PWD/hgrc ~/.hgrc
+
 [ -e "~/.gitconfig" ] && myexit "~/.gitconfig already exists..."
 
 ln -s $PWD/gitconfig ~/.gitconfig
