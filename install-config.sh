b4acf70b4c450ff844ef9691cb940e1fd147e6a6
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2014-10-06 14:58:55 -0400

    Added bash setup.

--- install-config.sh
+++ install-config.sh
@@ -5,6 +5,14 @@ myexit() {
     exit 1
 }
 
+[ -e "~/.bashrc" ] && myexit "~/.bashrc already exists..."
+
+ln -s $PWD/bashrc ~/.bashrc
+
+[ -e "~/.bashrc_aliases" ] && myexit "~/.bashrc_aliases already exists..."
+
+ln -s $PWD/bash/bashrc_aliases ~/.bashrc_aliases
+
 [ -e "~/.hgrc" ] && myexit "~/.hgrc already exists..."
 
 ln -s $PWD/hgrc ~/.hgrc
